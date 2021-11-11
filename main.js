c461c6e02fda948f8985bff73eace4a1e6618c30
Ale28814 <alened928@gmail.com>
2021-11-11 18:19:35 +0100

    correzione esercizio

--- main.js
+++ main.js
@@ -2,43 +2,68 @@
 //2-Dopo 30 secondi i numeri in pagina devono essere rimossi e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //3-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-//1- mostro i 5 numeri randomici nell'html
-const num1 = document.querySelector('.num-1').innerHTML = Math.floor(Math.random() * 51);
-const num2 = document.querySelector('.num-2').innerHTML = Math.floor(Math.random() * 51);
-const num3 = document.querySelector('.num-3').innerHTML = Math.floor(Math.random() * 51);
-const num4 = document.querySelector('.num-4').innerHTML = Math.floor(Math.random() * 51);
-const num5 = document.querySelector('.num-5').innerHTML = Math.floor(Math.random() * 51);
+//trovo i contenitori html
+const message = document.getElementById('message');
+const display = document.getElementById('display');
 
-const randomNumb = [num1, num2, num3, num4, num5];
+//genero i 5 numeri
+const randomNumbers = getRandomList(5);
+console.log(randomNumbers);
 
-//2- eseguo un timer di 30 sec
-/*const timeLeft = 30;
-const timer = setInterval(function() {
-    document.getElementById("timer").innerHTML = timeLeft + " secondi rimanenti";
-    timeLeft -= 1;
-    if (timeLeft <= 0) {
-        clearInterval(timer);
-        document.getElementById("timer").innerHTML = "end"
+display.innerHTML = randomNumbers;
+
+//creo i 30 sec di timer e allo scadere chiedo i numeri all'utente
+setTimeout(() => {
+    //cancello i numeri a schermo
+    display.innerHTML = '';
+    message.innerHTML = 'inserisci i numeri che ti ricordi';
+
+
+    setTimeout(() => {
+        //chiedo i 5 numeri all'utente
+        const userNumbers = getUserNumbers(randomNumbers.length);
+        console.log(userNumbers);
+
+        //controllo dei numeri
+        const guessed = userNumbers.filter(number => randomNumbers.includes(number));
+        console.log(guessed);
+
+        //risultato
+        message.innerHTML = `Hai indovinato ${guessed.length} numeri, su i numeri ${randomNumbers}`;
+        display.innerHTML = guessed;
+    }, 1000)
+
+
+
+}, 30000);
+
+function getRandomList(itemNumber) {
+    const randomNumbers = [];
+
+    while (randomNumbers.length < itemNumber) {
+        const rand = getRandomNumber(1, 100);
+
+        if (!randomNumbers.includes(rand)) {
+            randomNumbers.push(rand);
+        }
     }
-}, 1000)
+    return randomNumbers;
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
-//2.1 allo scadere del timer i numeri spariscono
-*/
+function getUserNumbers(askTimes) {
+    const numbers = [];
 
-//3- chiedi all'utente i 5 numeri apparsi
-const numOne = prompt('inserisci il primo numero dei 5');
-const numTwo = prompt('inserisci il secondo numero dei 5');
-const numThree = prompt('inserisci il terzo numero dei 5');
-const numFour = prompt('inserisci il quarto numero dei 5');
-const numFive = prompt('inserisci il quinto numero dei 5');
+    while (numbers.length < askTimes) {
+        const userNumber = parseInt(prompt(`Inserisci il numero ${numbers.length + 1} di ${askTimes}`));
 
-const userNumbers = [numOne, numTwo, numThree, numFour, numFive];
-console.log(userNumbers);
-
-//4- segnalare all'utente quanti e quali numeri ha indovinato
-if (userNumbers === randomNumb) {
-    alert('hai azzeccato tutti e 5 i numeri');
-} else {
-    alert('non hai azzeccato alcun numero');
-};
+        if (!numbers.includes(userNumber) && !isNaN(userNumber)) {
+            numbers.push(userNumber);
+        }
+    }
+    return numbers;
+}
